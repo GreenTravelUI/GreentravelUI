de1218106a958ffbac1531602c465a878f5f1ee4
amit <amit@ambait.com>
2016-01-22 18:59:16 +0530

    New Files From Shivangi ben

---
+++ GreenTravel/Controllers/IncomeBookController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace GreenTravel.Controllers
+{
+    public class IncomeBookController : Controller
+    {
+        //
+        // GET: /IncomeBook/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
