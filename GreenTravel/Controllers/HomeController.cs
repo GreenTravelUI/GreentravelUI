4b88448ec3be1c0ee3a8f542ad830aad91275f54
amit <amit@ambait.com>
2016-01-27 19:37:54 +0530

    2016-01-27 Evening

--- GreenTravel/Controllers/HomeController.cs
+++ GreenTravel/Controllers/HomeController.cs
@@ -23,112 +23,113 @@ namespace GreenTravel.Controllers
 
         public ActionResult Index()
         {
-            frm_para.FormType = "LoginPage"; //from_code
-            frm_para.corporate = "--None--";
-            frm_para.type = "Caption";
-            frm_para.FormTabCode = "--None--";
-            var form_val = _objcomman.GetFormData(frm_para);
+            //frm_para.FormType = "LoginPage"; //from_code
+            //frm_para.corporate = "--None--";
+            //frm_para.type = "Caption";
+            //frm_para.FormTabCode = "--None--";
+            //var form_val = _objcomman.GetFormData(frm_para);
 
-            string url = Request.Url.ToString();
-            FormValidationPara _FormValidationPara = new FormValidationPara()
-            {
-                type = "PageLoad",
-                url = "https://agent.travelzunlimited.com/crm"
-            };
+            //string url = Request.Url.ToString();
+            //FormValidationPara _FormValidationPara = new FormValidationPara()
+            //{
+            //    type = "PageLoad",
+            //    url = "https://agent.travelzunlimited.com/crm"
+            //};
 
-            Session["logo"] = "assets/images/logo-green.png";
-            Session["backgroundImage"] = "/assets/images/login-bg.jpg";
-            Session["Favicon"] = "assets/images/favicon.ico";
-            DataSet ds = ds = _objDBLogin.GetLoginData(_FormValidationPara);
-            if (ds.Tables[0] != null)
-            {
-                if (ds.Tables[0].Rows.Count > 0)
-                {
-                    ViewBag.Corporate = ds.Tables[0].Rows[0]["Corporate"];
-                    if (ds.Tables[0].Rows[0]["Facebook"] != null)
-                    {
-                        if (ds.Tables[0].Rows[0]["Facebook"].ToString() != "--None--" && ds.Tables[0].Rows[0]["Facebook"].ToString() != "")
-                            ViewBag.Facebook = ds.Tables[0].Rows[0]["Facebook"];
-                        else
-                            ViewBag.Facebook = "";
-                    }
-                    else
-                    {
-                        ViewBag.Facebook = "";
-                    }
+            //Session["logo"] = "assets/images/logo-green.png";
+            //Session["backgroundImage"] = "/assets/images/login-bg.jpg";
+            //Session["Favicon"] = "assets/images/favicon.ico";
+            //DataSet ds = ds = _objDBLogin.GetLoginData(_FormValidationPara);
+            //if (ds.Tables[0] != null)
+            //{
+            //    if (ds.Tables[0].Rows.Count > 0)
+            //    {
+            //        ViewBag.Corporate = ds.Tables[0].Rows[0]["Corporate"];
+            //        if (ds.Tables[0].Rows[0]["Facebook"] != null)
+            //        {
+            //            if (ds.Tables[0].Rows[0]["Facebook"].ToString() != "--None--" && ds.Tables[0].Rows[0]["Facebook"].ToString() != "")
+            //                ViewBag.Facebook = ds.Tables[0].Rows[0]["Facebook"];
+            //            else
+            //                ViewBag.Facebook = "";
+            //        }
+            //        else
+            //        {
+            //            ViewBag.Facebook = "";
+            //        }
 
-                    if (ds.Tables[0].Rows[0]["Twitter"] != null)
-                    {
-                        if (ds.Tables[0].Rows[0]["Twitter"].ToString() != "--None--" && ds.Tables[0].Rows[0]["Twitter"].ToString() != "")
-                            ViewBag.Twitter = ds.Tables[0].Rows[0]["Twitter"];
-                        else
-                            ViewBag.Twitter = "";
-                    }
-                    else
-                    {
-                        ViewBag.Twitter = "";
-                    }
+            //        if (ds.Tables[0].Rows[0]["Twitter"] != null)
+            //        {
+            //            if (ds.Tables[0].Rows[0]["Twitter"].ToString() != "--None--" && ds.Tables[0].Rows[0]["Twitter"].ToString() != "")
+            //                ViewBag.Twitter = ds.Tables[0].Rows[0]["Twitter"];
+            //            else
+            //                ViewBag.Twitter = "";
+            //        }
+            //        else
+            //        {
+            //            ViewBag.Twitter = "";
+            //        }
 
-                    if (ds.Tables[0].Rows[0]["GooglePlus"] != null)
-                    {
-                        if (ds.Tables[0].Rows[0]["GooglePlus"].ToString() != "--None--" && ds.Tables[0].Rows[0]["GooglePlus"].ToString() != "")
-                            ViewBag.GooglePlus = ds.Tables[0].Rows[0]["GooglePlus"];
-                        else
-                            ViewBag.GooglePlus = "";
-                    }
-                    else
-                    {
-                        ViewBag.GooglePlus = "";
-                    }
+            //        if (ds.Tables[0].Rows[0]["GooglePlus"] != null)
+            //        {
+            //            if (ds.Tables[0].Rows[0]["GooglePlus"].ToString() != "--None--" && ds.Tables[0].Rows[0]["GooglePlus"].ToString() != "")
+            //                ViewBag.GooglePlus = ds.Tables[0].Rows[0]["GooglePlus"];
+            //            else
+            //                ViewBag.GooglePlus = "";
+            //        }
+            //        else
+            //        {
+            //            ViewBag.GooglePlus = "";
+            //        }
 
-                    if (ds.Tables[0].Rows[0]["WebPortal"] != null)
-                    {
-                        if (ds.Tables[0].Rows[0]["WebPortal"].ToString() != "--None--" && ds.Tables[0].Rows[0]["WebPortal"].ToString() != "")
-                            ViewBag.WebPortal = ds.Tables[0].Rows[0]["WebPortal"];
-                        else
-                            ViewBag.WebPortal = "";
-                    }
-                    else
-                    {
-                        ViewBag.WebPortal = "";
-                    }
-
-
-
-                    if (ds.Tables[0].Rows[0]["backgroundImage"] != null)
-                    {
-                        if (ds.Tables[0].Rows[0]["backgroundImage"].ToString() != "" && ds.Tables[0].Rows[0]["backgroundImage"].ToString() != "--None--")
-                            Session["backgroundImage"] = ds.Tables[0].Rows[0]["backgroundImage"];
-                    }
-
-                    if (ds.Tables[0].Rows[0]["logo"] != null)
-                    {
-                        if (ds.Tables[0].Rows[0]["logo"].ToString() != "" && ds.Tables[0].Rows[0]["logo"].ToString() != "--None--")
-                            Session["logo"] = ds.Tables[0].Rows[0]["logo"];
-                    }
-
-                    if (ds.Tables[0].Rows[0]["LoginFrmCaption"] != null)
-                    {
-                        if (ds.Tables[0].Rows[0]["LoginFrmCaption"].ToString() != "" && ds.Tables[0].Rows[0]["LoginFrmCaption"].ToString() != "--None--")
-                            ViewBag.LoginFrmCaption = ds.Tables[0].Rows[0]["LoginFrmCaption"];
-                        else
-                            ViewBag.LoginFrmCaption = "";
-                    }
-                    else
-                    {
-                        ViewBag.LoginFrmCaption = "";
-                    }
-
-                    if (ds.Tables[0].Rows[0]["Favicon"] != null)
-                    {
-                        if (ds.Tables[0].Rows[0]["Favicon"].ToString() != "" && ds.Tables[0].Rows[0]["Favicon"].ToString() != "--None--")
-                            Session["Favicon"] = ds.Tables[0].Rows[0]["Favicon"];
-                    }
+            //        if (ds.Tables[0].Rows[0]["WebPortal"] != null)
+            //        {
+            //            if (ds.Tables[0].Rows[0]["WebPortal"].ToString() != "--None--" && ds.Tables[0].Rows[0]["WebPortal"].ToString() != "")
+            //                ViewBag.WebPortal = ds.Tables[0].Rows[0]["WebPortal"];
+            //            else
+            //                ViewBag.WebPortal = "";
+            //        }
+            //        else
+            //        {
+            //            ViewBag.WebPortal = "";
+            //        }
 
 
-                }
-            }
-            return View(form_val);
+
+            //        if (ds.Tables[0].Rows[0]["backgroundImage"] != null)
+            //        {
+            //            if (ds.Tables[0].Rows[0]["backgroundImage"].ToString() != "" && ds.Tables[0].Rows[0]["backgroundImage"].ToString() != "--None--")
+            //                Session["backgroundImage"] = ds.Tables[0].Rows[0]["backgroundImage"];
+            //        }
+
+            //        if (ds.Tables[0].Rows[0]["logo"] != null)
+            //        {
+            //            if (ds.Tables[0].Rows[0]["logo"].ToString() != "" && ds.Tables[0].Rows[0]["logo"].ToString() != "--None--")
+            //                Session["logo"] = ds.Tables[0].Rows[0]["logo"];
+            //        }
+
+            //        if (ds.Tables[0].Rows[0]["LoginFrmCaption"] != null)
+            //        {
+            //            if (ds.Tables[0].Rows[0]["LoginFrmCaption"].ToString() != "" && ds.Tables[0].Rows[0]["LoginFrmCaption"].ToString() != "--None--")
+            //                ViewBag.LoginFrmCaption = ds.Tables[0].Rows[0]["LoginFrmCaption"];
+            //            else
+            //                ViewBag.LoginFrmCaption = "";
+            //        }
+            //        else
+            //        {
+            //            ViewBag.LoginFrmCaption = "";
+            //        }
+
+            //        if (ds.Tables[0].Rows[0]["Favicon"] != null)
+            //        {
+            //            if (ds.Tables[0].Rows[0]["Favicon"].ToString() != "" && ds.Tables[0].Rows[0]["Favicon"].ToString() != "--None--")
+            //                Session["Favicon"] = ds.Tables[0].Rows[0]["Favicon"];
+            //        }
+
+
+            //    }
+            //}
+            //return View(form_val);
+            return View();
         }
 
         public string LoginUser(string Type, string Email, string url, string Pword)
