47ffb098d175c2e1d49d221fdddda66ce335891f
mihir2016 <mihir@ambait.com>
2016-02-19 10:22:15 +0530

    19-02-2016-mihir

--- GreenTravel/App_DbService/DBFromControlSetup.cs
+++ GreenTravel/App_DbService/DBFromControlSetup.cs
@@ -1,4 +1,5 @@
 ﻿using GreenTravel.Models;
+using GreenTravel.Models.Comman;
 using System;
 using System.Collections.Generic;
 using System.Data;
@@ -184,6 +185,42 @@ namespace GreenTravel.App_DbService
             {
 
                 throw;
+            }
+            finally
+            {
+                _cn.Close();
+                _cn.Dispose();
+            }
+        }
+
+        public DataSet BindDropDownLoad(CommanFieldPara CBP)
+        {
+            try
+            {
+                _cn.Open();
+                SqlCommand _cmd = new SqlCommand("sp_Formload_From_Field_Master", _cn);
+                _cmd.CommandType = CommandType.StoredProcedure;
+                _cmd.Parameters.AddWithValue("@Module", CBP.Module);
+                _cmd.Parameters.AddWithValue("@screen", CBP.screen);
+                _cmd.Parameters.AddWithValue("@FormCode", CBP.FormCode);
+                _cmd.Parameters.AddWithValue("@TabCode", CBP.TabCode);
+                _cmd.Parameters.AddWithValue("@Corporate", CBP.Corporate);
+                _cmd.Parameters.AddWithValue("@unit", CBP.unit);
+                _cmd.Parameters.AddWithValue("@Branch", CBP.Branch);
+                _cmd.Parameters.AddWithValue("@userid", CBP.userid);
+                _cmd.Parameters.AddWithValue("@Ip", CBP.Ip);
+                _cmd.Parameters.AddWithValue("@Type", "DropDown");
+                _cmd.CommandType = CommandType.StoredProcedure;
+                SqlDataAdapter _adp = new SqlDataAdapter(_cmd);
+                DataSet _ds = new DataSet();
+                _adp.Fill(_ds);
+                _adp.Dispose();
+                _cmd.Dispose();
+                return _ds;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
             }
             finally
             {
