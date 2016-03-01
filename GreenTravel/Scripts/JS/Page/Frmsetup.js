e4b4c27dcba973d14e5b56b0bb57847c3308f2b6
mihir2016 <mihir@ambait.com>
2016-03-01 17:40:48 +0530

    01-03-20161740

--- GreenTravel/Scripts/JS/Page/Frmsetup.js
+++ GreenTravel/Scripts/JS/Page/Frmsetup.js
@@ -1096,6 +1096,8 @@ $(document).ready(function () {
         clearForm();
         clearFormTAB();
         Quitform();
+        $('.tab3section1').hide();
+        $('.tab3Formname').text('');
         $('#btnUpdateFS').hide();
         $('#btnSavefs').show();
     });
@@ -1450,7 +1452,7 @@ function getdata() {
 }
 
 function getUtility() {
-    
+    htmlutility = '';
     var Module = '';
     var screen = '';
     var FormCode = '';
