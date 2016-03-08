3b3fbbc860a491dfca400eaae4077a2fc01bca89
khushbuambait <khushboo@ambait.com>
2016-03-08 19:28:41 +0530

    08-03-2016

---
+++ GreenTravel/Controllers/CorporateUserController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace GreenTravel.Controllers
+{
+    public class CorporateUserController : Controller
+    {
+        //
+        // GET: /CorporateUser/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
