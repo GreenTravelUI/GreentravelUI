940d1ae8106a8a3d7617287101fbcc5ba4bcd7a7
fefarparesh <paresh@ambait.com>
2016-04-04 13:03:23 +0530

    555

---
+++ GreenTravel/Controllers/AccountChartController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace GreenTravel.Controllers
+{
+    public class AccountChartController : Controller
+    {
+        //
+        // GET: /AccountChart/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
