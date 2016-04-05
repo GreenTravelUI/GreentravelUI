680f5db706ce080ded8eebd3351eacf6b39fafd8
mihir2016 <mihir@ambait.com>
2016-04-05 18:08:43 +0530

    05-04-2016-mihir

---
+++ GreenTravel/Controllers/GITTourListController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace GreenTravel.Controllers
+{
+    public class GITTourListController : Controller
+    {
+        //
+        // GET: /GITTourList/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
