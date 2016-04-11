eed4813d54684be255b9b49150a0d7f218670176
paresh <paresh@ambait.com>
2016-04-11 08:17:21 +0530

    hghg

---
+++ GreenTravel/Controllers/ImageMasterController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace GreenTravel.Controllers
+{
+    public class ImageMasterController : Controller
+    {
+        //
+        // GET: /ImageMaster/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
