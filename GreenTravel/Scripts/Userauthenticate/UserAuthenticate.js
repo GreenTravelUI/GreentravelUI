c44328adc0a7949f69be9672d12069bac08c05e8
fefarparesh <paresh@ambait.com>
2016-04-15 19:28:57 +0530

    15-04

---
+++ GreenTravel/Scripts/Userauthenticate/UserAuthenticate.js
@@ -0,0 +1,79 @@
+﻿$(document).ready(function () {
+
+    $('ol.sortable').nestedSortable({
+        forcePlaceholderSize: true,
+        handle: 'div',
+        helper: 'clone',
+        items: 'li',
+        opacity: .6,
+        placeholder: 'placeholder',
+        revert: 250,
+        tabSize: 25,
+        tolerance: 'pointer',
+        toleranceElement: '> div',
+        maxLevels: 3,
+
+        isTree: true,
+        expandOnHover: 700,
+        startCollapsed: true
+    });
+
+
+    if ($('ol.sortable').length) {
+        $('ol.sortable').nestedSortable({
+            forcePlaceholderSize: true,
+            handle: 'div',
+            helper: 'clone',
+            items: 'li',
+            opacity: .6,
+            placeholder: 'placeholder',
+            revert: 250,
+            tabSize: 25,
+            tolerance: 'pointer',
+            toleranceElement: '> div',
+            maxLevels: 3,
+            //isTree: true,
+            expandOnHover: 700,
+            startCollapsed: true
+        });
+
+        $('.disclose').on('click', function () {
+            $(this).closest('li').toggleClass('mjs-nestedSortable-collapsed').toggleClass('mjs-nestedSortable-expanded');
+        })
+
+        $('#serialize').click(function () {
+            serialized = $('ol.sortable').nestedSortable('serialize');
+            $('#serializeOutput').text(serialized + '\n\n');
+        })
+
+        $('#toHierarchy').click(function (e) {
+            hiered = $('ol.sortable').nestedSortable('toHierarchy', { startDepthCount: 0 });
+            hiered = dump(hiered);
+            (typeof ($('#toHierarchyOutput')[0].textContent) != 'undefined') ?
+            $('#toHierarchyOutput')[0].textContent = hiered : $('#toHierarchyOutput')[0].innerText = hiered;
+        })
+
+        $('#toArray').click(function (e) {
+            arraied = $('ol.sortable').nestedSortable('toArray', { startDepthCount: 0 });
+            arraied = dump(arraied);
+            (typeof ($('#toArrayOutput')[0].textContent) != 'undefined') ?
+            $('#toArrayOutput')[0].textContent = arraied : $('#toArrayOutput')[0].innerText = arraied;
+        })
+    }
+
+    $('.drag-close').click(function () {
+        $(this).parent().parent().remove();
+    });
+
+    $('#ulViewList').delegate("a", "click", function (e) {
+        $(this).parent().parent().find("li").removeClass("active");
+        $(this).parent().addClass("active");
+    });
+    $('.ulviewlistclass').delegate("a", "click", function (e) {
+        $(this).parent().parent().find("li").removeClass("active");
+        $(this).parent().addClass("active");
+    });
+
+
+
+});
