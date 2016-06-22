28ed22a5ae0f3b37199a976d550b0d329b0c124c
fefarparesh <paresh@ambait.com>
2016-06-22 19:59:32 +0530

    22-06-2016

--- GreenTravel/Scripts/TourGITQuotation/TourGITQuotation.js
+++ GreenTravel/Scripts/TourGITQuotation/TourGITQuotation.js
@@ -24,6 +24,8 @@ $(window).load(function () {
 
 $(document).ready(function () {
 
+
+
     $('#basicTree').jstree({
         'core': {
             'themes': {
@@ -367,3 +369,101 @@ function handle_click(e) {
     // your event code
 }
 
+$(function () {
+    //ajax mocks
+    $.mockjaxSettings.responseTime = 500;
+
+    $.mockjax({
+        url: '/post',
+        response: function (settings) {
+            log(settings, this);
+        }
+    });
+
+    $.mockjax({
+        url: '/error',
+        status: 400,
+        statusText: 'Bad Request',
+        response: function (settings) {
+            this.responseText = 'Please input correct value';
+            log(settings, this);
+        }
+    });
+
+    $.mockjax({
+        url: '/status',
+        status: 500,
+        response: function (settings) {
+            this.responseText = 'Internal Server Error';
+            log(settings, this);
+        }
+    });
+
+    $.mockjax({
+        url: '/groups',
+        response: function (settings) {
+            this.responseText = [
+             { value: 0, text: 'Guest' },
+             { value: 1, text: 'Service' },
+             { value: 2, text: 'Customer' },
+             { value: 3, text: 'Operator' },
+             { value: 4, text: 'Support' },
+             { value: 5, text: 'Admin' }
+            ];
+            log(settings, this);
+        }
+    });
+
+    function log(settings, response) {
+        var s = [], str;
+        s.push(settings.type.toUpperCase() + ' url = "' + settings.url + '"');
+        for (var a in settings.data) {
+            if (settings.data[a] && typeof settings.data[a] === 'object') {
+                str = [];
+                for (var j in settings.data[a]) { str.push(j + ': "' + settings.data[a][j] + '"'); }
+                str = '{ ' + str.join(', ') + ' }';
+            } else {
+                str = '"' + settings.data[a] + '"';
+            }
+            s.push(a + ' = ' + str);
+        }
+        s.push('RESPONSE: status = ' + response.status);
+
+        if (response.responseText) {
+            if ($.isArray(response.responseText)) {
+                s.push('[');
+                $.each(response.responseText, function (i, v) {
+                    s.push('{value: ' + v.value + ', text: "' + v.text + '"}');
+                });
+                s.push(']');
+            } else {
+                s.push($.trim(response.responseText));
+            }
+        }
+        s.push('--------------------------------------\n');
+        $('#console').val(s.join('\n') + $('#console').val());
+    }
+
+    //defaults
+    $.fn.editable.defaults.url = '/post';
+
+    var c = window.location.href.match(/c=inline/i) ? 'inline' : 'popup';
+    $.fn.editable.defaults.mode = c === 'inline' ? 'inline' : 'popup';
+
+    $('#c').val(c);
+    //enable / disable
+    $('#enable').click(function () {
+        $('#user .editable').editable('toggleDisabled');
+    });
+
+    //editables 
+    $('.username').editable({
+        url: '/post',
+        type: 'text',
+        pk: 1,
+        name: 'username',
+        title: 'Enter username'
+    });
+
+    
+});
